80da7163527c2e6be28d2b22402346fc43a3d53f
pratik kagale <pratikkagale3333@gmail.com>
2022-03-03 19:00:07 +0530

    Use of \ backslash

--- Practice/c.py
+++ Practice/c.py
@@ -10,3 +10,15 @@
 
 another_string = "another \n new line \n another line"
 print(another_string)
+
+#output:-
+# another
+#  new line
+#  another line
+
+
+new_string= "John said \"Hello\" to me"
+print(new_string)
+
+# output:-
+# John said "Hello" to me

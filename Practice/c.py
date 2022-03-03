fd59fdcc8a8c6b5a2d98656f0cb3cd9dcc297664
pratik kagale <pratikkagale3333@gmail.com>
2022-03-03 18:42:49 +0530

    use of \n

--- Practice/c.py
+++ Practice/c.py
@@ -2,8 +2,11 @@
 
  #Escape Character
 
- my_string = "long \
- sample text \
- more sample text"
+ # my_string = "long \
+ # sample text \
+ # more sample text"
+ #
+ # print(my_string)
 
- print(my_string)
+another_string = "another \n new line \n another line"
+print(another_string)
